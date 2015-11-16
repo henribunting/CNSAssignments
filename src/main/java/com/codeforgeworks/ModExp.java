095e68b40c3995c5e17dbecc4a5363842cf4ff9b
Morgan Geldenhuys <morgan@codeforgeworks.com>
2015-11-16 15:21:32 +0100

    add comments to modExp

--- src/main/java/com/codeforgeworks/ModExp.java
+++ src/main/java/com/codeforgeworks/ModExp.java
@@ -6,27 +6,31 @@ import java.math.BigInteger;
 import org.apache.commons.math3.random.RandomDataGenerator;
 import org.apache.commons.math3.analysis.function.Pow;
 
-import static java.lang.Math.pow;
-
+// Modular Exponentiation
 public class ModExp {
 
   // determine if a^b mod n = 1
   private static int modExp(int a, int b, int n) {
 
+    // get a char array of the bits in b
     char[] k = (Integer.toBinaryString(b)).toCharArray();
 
+    // initialize variables
     int c = 0, f = 1;
+    // loop over every bit in k
     for (int i = 0; i < k.length; i ++) {
 
       c *= 2;
       f = (f * f) % n;
 
+      // If current bit is equal to 1, then do this
       if (k[i] == '1') {
         c += 1;
         f = (f * a) % n;
       }
     }
 
+    // returns the remainder
     return f;
   }
 
